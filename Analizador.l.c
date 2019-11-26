95c463404978fb02d2a4ee16cfd22e74a539ad7e
yoselynv <49764140+yoselynv@users.noreply.github.com>
2019-11-26 00:41:09 -0600

    Indetificar operdores

---
+++ Analizador.l.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main () 
+{ 
+  char c, rutaFichero[255],id;
+  int  numeroPalabras = 0;
+  FILE *fichero;
+
+  
+  
+  printf ("\n\n\nIngrese el nombre del archivo:\n");
+  scanf (" %[^\n]", rutaFichero);
+  
+  fichero = fopen(rutaFichero, "r+");
+  while ((c = getc(fichero)) != EOF)
+  {
+    if (((c >= 'a') && (c <= 'z')) )
+	{
+	  id ='minuscula';
+	}
+	else if((c >= 'A') && (c <= 'Z')){
+    id='mayuscula';
+  }
+  else if((c >= '0') && (c <= '9')){
+  id="digito";
+ }
+ else if((c == '<') ||(c == '>') || (c=='<=')||(c=='>=')||(c=='==')){
+  id='relacional';
+ } else if((c == '+') ||(c == '-') || (c=='*')||(c=='/'){
+  id='operador';
+
+  printf("\n\n\nPulsa cualquier tecla para salir.");
+  system("PAUSE");
+  return 0;

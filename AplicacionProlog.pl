71ca7b8f19a9371703720b905c0767c3adb8f304
yoselynv <49764140+yoselynv@users.noreply.github.com>
2019-12-14 15:47:34 -0600

    Clase Aprobada.

--- AplicacionProlog.pl
+++ AplicacionProlog.pl
@@ -1,7 +1,16 @@
-
+imprimir_alumnos([]):-!.
 imprimir_alumnos([A|B]):-write(A),nl,imprimir_alumnos(B).
 imprimir_clases([A|B]):-write(A),nl,imprimir_clases(B).
-carrera([A|B],X).
+carrera(X).
 
-listaporcarrera([A|B],X):-carrera([A|B],X),imprimir_alumnos([A|B]).
-listaporclases([A|B],X):-carrera([A|B],X),imprimir_clases([A|B]).
+listaporcarrera([A|B],X):-carrera(X),imprimir_alumnos([A|B]).
+listaporclases([A|B],X):-carrera(X),imprimir_clases([A|B]).
+
+agregar(E,L,[E|L]).
+agregar(E,[X|Y],[X,Z]):-agregar(E,Y,Z).
+
+agregarcarrera(E,L,[E|L]).
+agregarcarrera(E,[X|Y],[X,Z]):-agregarcarrera(E,Y,Z).
+
+clase_aprobada(Z):-Z>64,write("Clase Aprobada").
+clase_aprobada(Z):-Z<65,write("Clase Reprobada").

d59e972a93a2cd64cc26c7d9206cd8c0a043ae93
yoselynv <49764140+yoselynv@users.noreply.github.com>
2019-12-14 14:49:00 -0600

    Lista de Alumnos por carrera.

---
+++ AplicacionProlog.pl
@@ -0,0 +1,6 @@
+
+imprimir_alumnos([A|B]):-write(A),nl,imprimir_alumnos(B).
+imprimir_clases([A|B]):-write(A),nl,imprimir_clases(B).
+carrera([A|B],X).
+listaporcarrera([A|B],X):-carrera([A|B],X),imprimir_alumnos([A|B]).
+listaporclases([A|B],X):-carrera([A|B],X),imprimir_clases([A|B]).

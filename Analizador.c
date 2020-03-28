35a744ff64963be4dd1ec57eb04c0efa0ed7bc4e
EstefanyMoncada <estefanymassiel.31@gmail.com>
2020-03-27 20:11:22 -0600

    Update Analizador.c

--- Analizador.c
+++ Analizador.c
@@ -1,34 +1,120 @@
-#include <stdio.h>
 
-int main () 
-{ 
-  char c, rutaFichero[255],id;
-  int  numeroPalabras = 0;
-  FILE *fichero;
+#include<stdio.h>
+#include<stdlib.h>
 
-  
-  
-  printf ("\n\n\nIngrese el nombre del archivo:\n");
-  scanf (" %[^\n]", rutaFichero);
-  
-  fichero = fopen(rutaFichero, "r+");
-  while ((c = getc(fichero)) != EOF)
-  {
-    if (((c >= 'a') && (c <= 'z')) )
-	
-	  id ='minuscula';
-	}
-	else if((c >= 'A') && (c <= 'Z')){
-    id='mayuscula';
-  }
-  else if((c >= '0') && (c <= '9')){
-  id="digito";
- }
- else if((c == '<') ||(c == '>') || (c=='<=')||(c=='>=')||(c=='==')){
-  id='relacional';
- }
+int main(){
+    FILE *PF;
+    char caracter;
+    pf=fopen;
 
-  printf("\n\n\nPulsa cualquier tecla para salir.");
-  system("PAUSE");
-  return 0;
+    char estado;
+    estado = '0'
+    printf("introduzca opcion");
+    scanf("%o" , &o);
+    switch (estado)
+    {
+    case '''':
+        if (estado=='a')
+        {
+            estado='l';
+        }else
+        {
+            if(estado=='l'){
+               estado='j';
+            }
+        }
+        else
+        {
+            if(estado=='j'){
+                estado='k'
+            }
+        }
+        break;
+    case 'letra_mayuscula'
+        if (estado=='j' || estado=='k')
+        {
+            estado='j'
+        }
+        break;
+
+    case 'alfa':
+         if (estado=='j' || estado=='k')
+        {
+            estado='k'
+        }
+        break;
+
+    case ' digito':
+        if(estado=='j'){
+            estado='k';
+        }
+        break;
+    case '<-':
+          if(estado=='b'){
+              estado='j'
+          }
+          else
+          {
+              if(estado=='j'){
+                  estado='j'
+              }
+          }
+          break;
+
+    case 'ope_relacional':
+          if(estado=='c'|| estado=='i'){
+              estado='c';
+          }
+          else
+          {
+              if(estado=='lr'){
+                  estado='c';
+              }
+          }
+          break;
+
+    case 'condicion':
+           if(estado=='d'){
+               estado='d';
+           } 
+           else{
+               if(estado=='d' || estado =='i'){
+                   estado='si';
+               }
+           }
+           break;     
+    case '"':
+         if(estado=='e' || estado=='i'){
+             estado='e';
+         } 
+         else
+         {
+             if(estado=='f' || estado=='i'){
+                 estado='f';
+             }
+
+         }  
+
+
+          
+            
+
+
+    case '\0':
+         if(estado=='a') [
+
+         ]   
+            
+        
+        
+         
+
+        
+                             
+    
+    default: printf("error, no es una instruccion")
+        break;
+    }
+
 }
+    
